9acbab9ece656277437032bdd34ca6054597da7a
amirtbi <torabi46@gmail.com>
2022-12-30 21:07:26 +0330

    Merge factory pattern example from builder pattern repository

--- src/builder-pattern.ts
+++
@@ -1,9 +0,0 @@
-// Builder pattern
-
-// class User {
-//   constructor(name,age,sex,height,nationality) {
-//     this.name = userInfo.name;
-//     this.age = userInfo.age;
-//     this.sex = userInfo.
-//   }
-// }
