d47f2b4fc10161a4f893f69c89974546acfee77e
amirtbi <torabi46@gmail.com>
2022-12-30 22:42:11 +0330

    Define builder pattern

--- dist/builder-pattern.js
+++ dist/builder-pattern.js
@@ -1,10 +1,33 @@
 "use strict";
 // Builder pattern
-// class User {
-//   constructor(name,age,sex,height,nationality) {
-//     this.name = userInfo.name;
-//     this.age = userInfo.age;
-//     this.sex = userInfo.
-//   }
-// }
+class Car {
+    constructor(CarBuilder) {
+        this.seats = CarBuilder.seats;
+        this.color = CarBuilder.color;
+        this.doors = CarBuilder.doors;
+    }
+}
+class Builder {
+    setSeats(seats) {
+        this.seats = seats;
+        return this;
+    }
+    setColor(color) {
+        this.color = color;
+        return this;
+    }
+    setDoor(doors) {
+        this.doors = doors;
+        return this;
+    }
+    build() {
+        return new Car(this);
+    }
+}
+// Build  a Car
+const car1 = new Builder().setSeats(4).setColor("red").setDoor(2).build();
+console.log("Car build instance", car1);
+//Build a Car with 4 doors and black color
+const car2 = new Builder().setSeats(4).setColor("black").setDoor(4).build();
+console.log("Car build instance 2", car2);
 //# sourceMappingURL=builder-pattern.js.map
--- src/builder-pattern.ts
+++ src/builder-pattern.ts
@@ -1,9 +1,57 @@
 // Builder pattern
 
-// class User {
-//   constructor(name,age,sex,height,nationality) {
-//     this.name = userInfo.name;
-//     this.age = userInfo.age;
-//     this.sex = userInfo.
-//   }
-// }
+interface IBuilder {
+  setSeats(seats: number): this;
+  setColor(color: string): this;
+  setDoor(doors: number): this;
+}
+
+class Car {
+  public seats: number;
+  public color: string;
+  public doors: number;
+
+  constructor(CarBuilder: Builder) {
+    this.seats = CarBuilder.seats;
+    this.color = CarBuilder.color;
+    this.doors = CarBuilder.doors;
+  }
+}
+
+class Builder implements IBuilder {
+  // Fields
+  public doors: number;
+  public seats: number;
+  public color: string;
+
+  setSeats(seats: number): this {
+    this.seats = seats;
+    return this;
+  }
+
+  setColor(color: string): this {
+    this.color = color;
+    return this;
+  }
+
+  setDoor(doors: number): this {
+    this.doors = doors;
+    return this;
+  }
+
+  build(): Car {
+    return new Car(this);
+  }
+}
+
+// Build  a Car
+
+const car1 = new Builder().setSeats(4).setColor("red").setDoor(2).build();
+
+console.log("Car build instance", car1);
+
+//Build a Car with 4 doors and black color
+
+const car2 = new Builder().setSeats(4).setColor("black").setDoor(4).build();
+
+console.log("Car build instance 2", car2);
